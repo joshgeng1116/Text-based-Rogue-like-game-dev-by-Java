4ccdb517db8b2436572f0581b55efca04d3cdc45
slim0105 <slim0105@student.monash.edu>
2021-09-24 13:16:53 +1000

    Implemented Vendor Buy Giant Axe and Broadsword actions

--- src/game/VendorBuyBroadswordAction.java
+++ src/game/VendorBuyBroadswordAction.java
@@ -6,8 +6,14 @@ import edu.monash.fit2099.engine.GameMap;
 public class VendorBuyBroadswordAction extends VendorBuyAction{
     @Override
     public String execute(Actor actor, GameMap map) {
-        return super.execute(actor, map);
-    }
+        Player player = (Player) actor;
+        if (player.getSouls() > Broadsword.cost()){
+            player.subtractSouls(Broadsword.cost());
+            return SwapWeaponAction(Broadsword); //need a reference to the Broadsword
+        }else{
+            return "Not enough Souls to purchase Broadsword";
+        }}
+
 
     @Override
     public String menuDescription(Actor actor) {
--- src/game/VendorBuyGiantAxeAction.java
+++ src/game/VendorBuyGiantAxeAction.java
@@ -6,12 +6,13 @@ import edu.monash.fit2099.engine.GameMap;
 public class VendorBuyGiantAxeAction extends VendorBuyAction{
     @Override
     public String execute(Actor actor, GameMap map) {
-
-            return SwapWeaponAction(GiantAxe);}
-
-
-
-
+        Player player = (Player) actor;
+        if (player.getSouls() > GiantAxe.cost()){
+            player.subtractSouls(GiantAxe.cost());
+            return SwapWeaponAction(GiantAxe); //need a reference to the Giant Axe
+        }else{
+            return "Not enough Souls to purchase Giant Axe";
+        }}
 
     @Override
     public String menuDescription(Actor actor) {
