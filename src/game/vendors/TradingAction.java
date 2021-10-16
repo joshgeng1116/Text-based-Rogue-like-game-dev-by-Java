787fd4607880af5ae9cf87b2d770f2ea169a230f
slim0105 <slim0105@student.monash.edu>
2021-10-16 18:53:08 +1100

    Fixed trading action class

--- src/game/vendors/TradingAction.java
+++ src/game/vendors/TradingAction.java
@@ -3,15 +3,21 @@ package game.vendors;
 import edu.monash.fit2099.engine.Action;
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
+import game.enemies_and_behaviours.LordOfCinder;
 
 public class TradingAction extends Action {
+    LordOfCinder boss;
+    public TradingAction(LordOfCinder boss) {
+        this.boss = boss;
+    }
+
     @Override
     public String execute(Actor actor, GameMap map) {
-        return null;
+        return null; /* unable to progress cos lord of cinder classes not implemented*/
     }
 
     @Override
     public String menuDescription(Actor actor) {
-        return null;
-    }
+        return "Unkindled trades Cinder of "+ boss.name();}/* Unkindled trades Cinder of Aldrich the Devourer/Yhorm the Giant*/
+
 }
